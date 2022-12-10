9ed01f74df93dba9b94097f436b38bd9bb8f7aab
Andrew Litvinov <lita.private@gmail.com>
2022-12-10 23:14:13 +0300

    feat: commands os added

--- handles/os-operations.js
+++ handles/os-operations.js
@@ -1,3 +1,28 @@
-export const osInfo = (source, target) => {
-  console.log('not implemented');
+import os from 'os';
+import { fmMessage } from '../lib/utils.js';
+export const osInfo = (arg) => {
+  //   os --EOL
+  // os --cpus
+  // os --homedir
+  // os --username
+  // os --architecture
+  switch (arg) {
+    case '--EOL':
+      fmMessage(JSON.stringify(os.EOL));
+      break;
+    case '--cpus':
+      console.table(os.cpus());
+      break;
+    case '--homedir':
+      fmMessage(os.homedir());
+      break;
+    case '--username':
+      fmMessage(os.userInfo().username);
+      break;
+    case '--arhitecture':
+      fmMessage(os.arch);
+      break;
+    default:
+      break;
+  }
 };
